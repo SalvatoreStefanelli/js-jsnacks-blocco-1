bb0aab79e8a274eb0c5bab8a16042ed60f077f2d
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2023-12-03 22:32:39 +0100

    create array and the prompt

---
+++ jsnack4/script.js
@@ -0,0 +1,19 @@
+//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no
+// alla festa.
+
+// Creo un array con una lista di invitati
+const list = ['Salvatore', 'Christian', 'Dario', 'Davide', 'Giuseppe', 'Biagio'];
+
+// Creo un prompt per chiedere all'utente il suo nome
+const name = prompt('What\s your name?');
+
+
+// Creo una condizione per vedere se l'utente è presente nella lista
+
+// for (let i = 0; i < list.length; i++) {
+//     const element = list[i];
+    
+// }
+// if (name  ==) {
+//     console.log('you are invitated');
+// }

1d96db737af0a39ba83d5f21060367ade0814931
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2023-12-04 18:25:27 +0100

    add prompt of number

---
+++ jsnack8/script.js
@@ -0,0 +1,7 @@
+//Chiedi un numero di 4 cifre all’utente
+//e calcola la somma di tutte le cifre che compongono il numero.
+
+// Uso un prompt per chiedere un numero di 4 cifre all'utente
+
+const numb = Number(prompt('Type a 4-digit number'));
+
